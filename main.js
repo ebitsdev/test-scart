771cd793bff156ac195f051442492d53bbfb7a61
E BITSDev <me@ebamba.com>
2024-08-10 19:57:14 -0400

    Added new javascript file

---
+++ main.js
@@ -0,0 +1,13 @@
+function getArrayElements(arr){
+    arr.forEach(element => {
+        console.log(element)
+    });
+}
+
+const exampleArray = [
+    'Orange',
+    'Apple',
+    'Grape',
+    'Strawberry'
+]
+getArrayElements(exampleArray);
